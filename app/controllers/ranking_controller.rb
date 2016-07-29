39f1ac25441be4392056f90ee39f5850f4d90b23
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-07-29 08:48:28 +0000

    updated on mentoring

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -11,3 +11,7 @@ class RankingController < ApplicationController
         render 'show'
     end
 end
+
+# @ranking = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id')
+# item_ids = @ranking.keys
+# @items = Item.find(item_ids).sort_by{|o| item_ids.index(o.id)}
